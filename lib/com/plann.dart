5357f024ee4b32e74707e50f95f42697a6328602
levi1405 <108413228+levi1405@users.noreply.github.com>
2023-10-23 05:39:03 -0600

    Creacion de Pantalla card Planetas

---
+++ lib/com/plann.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Plannnn extends StatelessWidget {
+  const Plannnn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
